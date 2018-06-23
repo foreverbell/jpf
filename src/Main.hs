765abc2858acab9a449950c2fd48cf5579e45ef0
foreverbell <dql.foreverbell@gmail.com>
2018-06-23 17:36:57 +0800

    bugfix: plain block has the least parse priority.
    Also run `stylish-haskell`.

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import qualified Text.Parsec as P
+import           Control.Monad    (unless)
+import qualified Data.Map.Strict  as M
+import           Data.Maybe       (fromJust)
+import           System.IO        (isEOF)
+import qualified Text.Parsec      as P
 import qualified Text.Parsec.Char as P
-import qualified Data.Map.Strict as M
-import System.IO (isEOF)
-import Control.Monad (unless)
-import Data.Maybe (fromJust)
 
-import qualified Kana as K
+import qualified Kana             as K
 
 type Parser = P.Parsec String ()
 
@@ -21,7 +21,10 @@ acceptedWords :: [String]
 acceptedWords = ["`", "'", "-"] ++ K.syllables
 
 plain :: Parser String
-plain = P.many1 $ P.satisfy (/= '$')
+plain = do
+  x <- P.anyChar
+  xs <- P.many $ P.satisfy (/= '$')
+  return (x:xs)
 
 word :: Parser String
 word = P.choice $ map (P.try . P.string) acceptedWords
@@ -37,7 +40,10 @@ kata = do
   P.between (P.char '(') (P.char ')') (P.many1 word)
 
 top :: Parser [TextAST]
-top = P.many $ P.choice [Plain <$> plain, Hira <$> hira, Kata <$> kata]
+top = do
+  xs <- P.many $ P.choice [Hira <$> hira, Kata <$> kata, Plain <$> plain]
+  P.eof
+  return xs
 
 pprint :: [TextAST] -> String
 pprint xs = concatMap pprintHelper xs
@@ -45,22 +51,23 @@ pprint xs = concatMap pprintHelper xs
     pprintHelper (Plain p) = p
     pprintHelper (Hira rs) = concatMap (pprintRomaji K.hiraMap) rs
     pprintHelper (Kata rs) = concatMap (pprintRomaji K.kataMap) rs
-    
-    pprintRomaji lookupMap r = 
+
+    pprintRomaji lookupMap r =
       case r of
         "`" -> "「"
         "'" -> "」"
         "-" -> "ー"
-        _ -> fromJust $ M.lookup r lookupMap
-    
+        _   -> fromJust $ M.lookup r lookupMap
+
 main :: IO ()
 main = loop
-  where 
+  where
     loop = do
       done <- isEOF
       unless done $ do
         inp <- getLine
         case P.runParser top () "" inp of
-          Left e -> putStrLn (show e)
-          Right a -> putStrLn (pprint a)
-        loop
+          Left e  -> putStrLn (show e)
+          Right a -> do
+            putStrLn (pprint a)
+            loop
